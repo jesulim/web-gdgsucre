abb677a1106758ee9c3e827048845acd51884158
pablonoya <pablonoya18@live.com>
2025-07-10 21:25:39 -0400

    Add supabase, update dependencies

---
+++ src/lib/supabase.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js"
+
+export const supabase = createClient(
+  import.meta.env.SUPABASE_URL,
+  import.meta.env.SUPABASE_ANON_KEY,
+  {
+    auth: {
+      flowType: "pkce",
+      autoRefreshToken: true,
+      persistSession: true,
+    },
+  }
+)
