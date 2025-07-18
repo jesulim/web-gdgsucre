d92848ae5e093eb13c606b1efe51b516ca627cbb
pablonoya <pablonoya18@gmail.com>
2025-07-18 19:00:33 -0400

    Add user signout with supabase client

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -1,6 +1,10 @@
+import { createUserClient } from "@/lib/supabase"
 import type { APIRoute } from "astro"
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
+  const supabase = await createUserClient(cookies)
+
+  await supabase.auth.signOut()
   cookies.delete("sb-access-token", { path: "/" })
   cookies.delete("sb-refresh-token", { path: "/" })
 
