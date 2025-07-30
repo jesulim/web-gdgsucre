5124e26bf1414ba336d914130b8b628cc73259a4
pablonoya <pablonoya18@gmail.com>
2025-07-30 15:05:58 -0400

    Add middleware for session refreshing

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { defineMiddleware } from "astro:middleware"
+import { supabase } from "@/lib/supabase"
+import { setSupabaseCookies } from "./lib/utils"
+
+export const onRequest = defineMiddleware(async (context, next) => {
+  const { cookies, url } = context
+  const path = url.pathname
+
+  if (path.startsWith("/api/auth")) return next()
+
+  const accessToken = cookies.get("sb-access-token")?.value
+  const refreshToken = cookies.get("sb-refresh-token")?.value
+
+  if (!accessToken && refreshToken) {
+    const { data } = await supabase.auth.refreshSession({
+      refresh_token: refreshToken,
+    })
+
+    if (data?.session) {
+      const { access_token, refresh_token, expires_in } = data.session
+      setSupabaseCookies(cookies, access_token, refresh_token, expires_in)
+    }
+  }
+
+  return next()
+})
--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -1,7 +1,7 @@
 import { supabase } from "@/lib/supabase"
-import type { APIRoute } from "astro"
+import { setSupabaseCookies } from "@/lib/utils"
 
-const sevenDays = 60 * 60 * 24 * 7
+import type { APIRoute } from "astro"
 
 export const GET: APIRoute = async ({ url, cookies, redirect }) => {
   const authCode = url.searchParams.get("code")
@@ -18,20 +18,7 @@ export const GET: APIRoute = async ({ url, cookies, redirect }) => {
   }
 
   const { access_token, refresh_token, expires_in } = data.session
-
-  cookies.set("sb-access-token", access_token, {
-    path: "/",
-    httpOnly: true,
-    secure: import.meta.env.PROD,
-    maxAge: expires_in,
-  })
-
-  cookies.set("sb-refresh-token", refresh_token, {
-    path: "/",
-    httpOnly: true,
-    secure: import.meta.env.PROD,
-    maxAge: sevenDays,
-  })
+  setSupabaseCookies(cookies, access_token, refresh_token, expires_in)
 
   return redirect(next)
 }
