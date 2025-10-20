c258a30ff7b1389b4b79e68c2c50278e960fe5c1
pablonoya <pablonoya18@gmail.com>
2025-10-20 19:28:46 -0400

    Add default fields to organizerService

--- src/lib/services/organizersService.ts
+++ src/lib/services/organizersService.ts
@@ -6,14 +6,12 @@ export async function getOrganizers(supabase: SupabaseClient, eventSlug: string)
     .select("id, profiles (first_name, last_name, avatar_url), events(slug)")
     .eq("events.slug", eventSlug)
 
-  if (error) {
-    console.error("Error fetching organizers:", error)
-    return null
-  }
+  if (error) return null
 
   return data.map(organizer => ({
     id: organizer.id,
-    image: organizer.profiles.avatar_url,
-    ...organizer.profiles,
+    image: organizer.profiles?.avatar_url ?? null,
+    first_name: organizer.profiles?.first_name ?? "",
+    last_name: organizer.profiles?.last_name ?? "",
   }))
 }
