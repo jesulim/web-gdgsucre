65b8917fa7ab2eb920c9d34c9bd418f49d87431e
pablonoya <pablonoya18@gmail.com>
2025-10-28 22:52:59 -0400

    Update

---
+++ src/components/events/devfest-25/speaker/SpeakerCard.tsx
@@ -0,0 +1,31 @@
+export interface Props {
+  id: string
+  title: string
+  img: string
+  name: string
+  description: string
+}
+
+export const SpeakerCard = ({ id, title, img, name, description }: Props) => {
+  return (
+    <div key={id} className="card-speaker flex flex-col">
+      <div className="mb-2 flex flex-col h-full rounded-lg shadow-md overflow-hidden">
+        <h1 className="text-xl">{title}</h1>
+      </div>
+
+      <div className="relative">
+        <img
+          src={img}
+          alt={name}
+          className="rounded-lg w-full object-cover"
+          loading="lazy"
+          decoding="async"
+        />
+        <div className="absolute bottom-2 left-2">
+          <h2 className="text-lg font-semibold">{name}</h2>
+          <p className="text-sm text-red-500">{description}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
