c619d663a732df532eed1d88d62d0906bd86e547
M4x241 <maxrodaspc@gmail.com>
2026-02-26 19:01:24 -0400

    Cards con datos actualizados

--- src/components/events/iwd-26/speakers/SpeakerCard.tsx
+++ src/components/events/iwd-26/speakers/SpeakerCard.tsx
@@ -9,21 +9,21 @@ export interface Props {
 
 export const SpeakerCard = ({ id, title, img, name, description, skills }: Props) => {
   return (
-    <div className="card-speaker flex flex-col rounded-lg overflow-hidden shadow-lg">
+    <div className="card-speaker flex flex-col rounded-lg overflow-hidden shadow-lg ">
       <div className="mb-2 flex flex-col h-full overflow-hidden"></div>
       <div className="relative">
         <img
           src={img}
           alt={name}
-          className="w-full object-cover"
-          width="340"
-          height="301"
+          className="w-full object-cover h-[330px] object-center rounded-t-lg"
+          width="auto"
+          height="330"
           loading="lazy"
           decoding="async"
         />
       </div>
-      <div className="p-4">
-        <div className="text-sm font-semibold text-blue-500 mb-1" id="skills">
+      <div className="p-4 ">
+        <div className="text-sm text-[#1976D2] font-semibold mb-1" id="skills">
           {skills &&
             skills.map(skill => (
               <span
@@ -35,8 +35,8 @@ export const SpeakerCard = ({ id, title, img, name, description, skills }: Props
             ))}
         </div>
         <h1 className="text-xl font-semibold">{name}</h1>
-        <h2 className="text-sm text-gray-500">@CTO TechFlow</h2>
-        <p className="text-sm text-gray-600 mt-2 line-clamp-3 leading-relaxed">{title}</p>
+        <h2 className="text-sm text-gray-800">@CTO TechFlow</h2>
+        <p className="text-sm text-gray-500 mt-2 line-clamp-3 leading-relaxed">{description}</p>
       </div>
     </div>
   )
