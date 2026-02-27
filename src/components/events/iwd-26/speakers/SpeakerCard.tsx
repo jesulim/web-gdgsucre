327d8855e0a97898f415bfc2c996c0321ed9f1c5
Max <160605977+M4x241@users.noreply.github.com>
2026-02-27 01:19:29 -0400

    Feat/speakers section (#94)
    * Add-Speakers-Section
    
    * Cards con datos actualizados
    
    * Cards change tags with Cities
    
    * fix Errores en Speakers Section
    
    * fix Errores en Speakers Section-v2
    
    * fix Errores en Speakers Section-v3

---
+++ src/components/events/iwd-26/speakers/SpeakerCard.tsx
@@ -0,0 +1,46 @@
+export interface Props {
+  id: string
+  title: string
+  ciudad: string
+  img: string
+  name: string
+  description: string
+  description_summarize: string
+  skills?: { id: string; name: string }[]
+}
+
+export const SpeakerCard = ({ ciudad, img, name, description_summarize, skills }: Props) => {
+  return (
+    <div className="card-speaker flex flex-col rounded-lg overflow-hidden shadow-lg h-full">
+      <div className="relative">
+        <img
+          src={img}
+          alt={name}
+          className="w-full object-cover h-[330px] object-center rounded-t-lg"
+          width="400"
+          height="330"
+          loading="lazy"
+          decoding="async"
+        />
+      </div>
+      <div className="p-4">
+        <div className="text-sm text-[#1976D2] font-semibold mb-1">
+          {skills &&
+            skills.map(skills => (
+              <span
+                key={skills.id}
+                className="inline-block bg-white rounded-full px-2 py-1 text-xs mr-2 mb-1 border border-blue-500"
+              >
+                {skills.name}
+              </span>
+            ))}
+        </div>
+        <h1 className="text-xl font-semibold">{name}</h1>
+        <h2 className="text-sm text-gray-800">{ciudad}</h2>
+        <p className="text-sm text-gray-500 mt-2 line-clamp-10 leading-relaxed">
+          {description_summarize}
+        </p>
+      </div>
+    </div>
+  )
+}
