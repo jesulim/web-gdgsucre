a6f11e2a5fff6ce4623724a5e0bb8960e92f1c53
Max <160605977+M4x241@users.noreply.github.com>
2026-03-07 01:38:43 -0400

    Feat/section organizers (#101)
    * Add-Speakers-Section
    
    * Cards con datos actualizados
    
    * Cards change tags with Cities
    
    * fix Errores en Speakers Section
    
    * fix Errores en Speakers Section-v2
    
    * fix Errores en Speakers Section-v3
    
    * feat/Section-Organizers
    
    * Se anadio la seccion de organizers
    
    * fix: movil version overflow
    
    * fix: movil version - organizers card responsive
    
    * fix: hidden button - version movil
    
    * Add: Organizers faltantes
    
    * Update organizers, comment out lunch in QR scanner
    
    ---------
    
    Co-authored-by: Pablo Noya <pablonoya18@gmail.com>

---
+++ src/components/events/iwd-26/organizers/OrganizerCard.tsx
@@ -0,0 +1,69 @@
+import MarcoSVG from "@/assets/events/iwd-26/patronOrganizers.svg"
+
+export interface Props {
+  imageUrl: string | null
+  img: string
+  full_name: string
+  carrera: string
+  areas?: string[]
+}
+
+export const OrganizerCard = ({ img, full_name, areas }: Props) => {
+  return (
+    <div className="card-speaker flex flex-col rounded-lg overflow-hidden shadow-lg h-full relative">
+      <div className="relative">
+        <img
+          src={img}
+          alt={full_name}
+          className="w-full object-cover h-[300px] md:h-[300px] object-center rounded-t-lg"
+          width="400"
+          height="400"
+          loading="lazy"
+          decoding="async"
+          style={{
+            borderBottom: "4px solid #EBBA3C",
+          }}
+        />
+      </div>
+      <div className="pt-1 pb-8 flex flex-col items-center gap-2 md:gap-4 text-center">
+        <h3 className="text-xl md:text-2xl font-semibold">
+          {full_name.split(" ").length > 2 ? (
+            <>
+              {full_name.split(" ").slice(0, 2).join(" ")} <br />
+              {full_name.split(" ").slice(2).join(" ")}
+            </>
+          ) : full_name.split(" ").length === 2 ? (
+            <>
+              {full_name.split(" ")[0]} <br />
+              {full_name.split(" ")[1]}
+            </>
+          ) : (
+            <>
+              {full_name} <br />
+              {"\u200B"}
+            </>
+          )}
+        </h3>
+        <div className="text-sm md:text-base text-[#1976D2] font-semibold mb-1">
+          {areas &&
+            areas.map(area => (
+              <span
+                key={area}
+                className="inline-block bg-[#0333AB] rounded-full px-2 py-1 text-xs md:text-sm mr-2 mb-1 border border-[#0333AB] text-white"
+              >
+                {area}
+              </span>
+            ))}
+        </div>
+      </div>
+      <div>
+        <img
+          src={MarcoSVG.src}
+          alt=""
+          aria-hidden="true"
+          className="absolute bottom-0 left-0 w-full"
+        />
+      </div>
+    </div>
+  )
+}
