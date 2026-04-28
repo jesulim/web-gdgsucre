ba117df24595ef46a960f3b707d79bd0d6d15e87
pablonoya <pablonoya18@gmail.com>
2026-04-27 22:06:01 -0400

    Add api/teams endpoints

--- src/pages/api/teams/create.ts
+++ src/pages/api/teams/create.ts
@@ -2,7 +2,8 @@ import type { APIRoute } from "astro"
 
 import { sendRegistrationConfirmationEmail } from "@/lib/services/emailService"
 import { createProfile, getProfile } from "@/lib/services/profileService"
-import { getRegistrationByUser, submitRegistration } from "@/lib/services/registrationService"
+import { submitRegistration } from "@/lib/services/registrationService"
+import { createTeam } from "@/lib/services/teamService"
 import { createUserClient } from "@/lib/supabase"
 
 export const POST: APIRoute = async ({ request, cookies }) => {
@@ -20,56 +21,42 @@ export const POST: APIRoute = async ({ request, cookies }) => {
     ...fields
   } = Object.fromEntries(formData)
 
-  if (first_name && last_name && phone_number) {
-    try {
-      await createProfile(supabase, String(first_name), String(last_name), String(phone_number))
-    } catch (error) {
-      return new Response(`Error al crear el perfil: ${error}`, {
-        status: 500,
-      })
-    }
-  }
-
   try {
-    const {
-      data: { user },
-    } = await supabase.auth.getUser()
-    if (!user) return new Response("No autorizado", { status: 401 })
-
-    const existingRegistration = await getRegistrationByUser(supabase, user.id, String(event_slug))
-    let isNewRegistration = false
-
-    if (!existingRegistration) {
-      await submitRegistration(supabase, {
-        event_id: String(event_id),
-        event_slug: String(event_slug),
-        fields,
-      })
-      isNewRegistration = true
+    if (first_name && last_name && phone_number) {
+      await createProfile(supabase, String(first_name), String(last_name), String(phone_number))
     }
 
-    // TODO: replace with createTeam func
-    console.log(`Creando team: ${team_name} para el evento: ${event_id}`)
+    await submitRegistration(supabase, {
+      event_id: String(event_id),
+      event_slug: String(event_slug),
+      fields,
+    })
 
-    if (isNewRegistration) {
-      const userProfile = await getProfile(supabase)
+    await createTeam(supabase, {
+      event_id: Number(event_id),
+      name: String(team_name),
+    })
 
-      if (userProfile && "email" in userProfile) {
-        await sendRegistrationConfirmationEmail({
-          userEmail: userProfile.email,
-          userName: userProfile.first_name ?? "",
-          eventName: String(event_name),
-          eventSlug: String(event_slug),
-        })
-      } else {
-        console.error("No se pudo obtener el perfil o el email del usuario para enviar el correo.")
-      }
+    const userProfile = await getProfile(supabase)
+
+    if (userProfile && "email" in userProfile) {
+      await sendRegistrationConfirmationEmail({
+        userEmail: userProfile.email,
+        userName: userProfile.first_name ?? "",
+        eventName: String(event_name),
+        eventSlug: String(event_slug),
+      })
+    } else {
+      console.error("No se pudo obtener el perfil o el email del usuario para enviar el correo.")
     }
 
     return new Response("Registro exitoso", { status: 200 })
-  } catch (error: any) {
-    return new Response(`Error al registrar y crear equipo: ${error.message || error}`, {
-      status: 500,
-    })
+  } catch (error) {
+    return new Response(
+      `Error al registrar y crear equipo: ${error instanceof Error ? error.message : error}`,
+      {
+        status: 500,
+      }
+    )
   }
 }
--- src/pages/api/teams/join.ts
+++ src/pages/api/teams/join.ts
@@ -3,6 +3,7 @@ import type { APIRoute } from "astro"
 import { sendRegistrationConfirmationEmail } from "@/lib/services/emailService"
 import { createProfile, getProfile } from "@/lib/services/profileService"
 import { getRegistrationByUser, submitRegistration } from "@/lib/services/registrationService"
+import { joinTeam } from "@/lib/services/teamService"
 import { createUserClient } from "@/lib/supabase"
 
 export const POST: APIRoute = async ({ request, cookies }) => {
@@ -23,56 +24,42 @@ export const POST: APIRoute = async ({ request, cookies }) => {
   // TODO: validate team code exists before registering the user, block if invalid
   console.log(`team code: ${team_code} para el evento: ${event_id}`)
 
-  if (first_name && last_name && phone_number) {
-    try {
-      await createProfile(supabase, String(first_name), String(last_name), String(phone_number))
-    } catch (error) {
-      return new Response(`Error al crear el perfil: ${error}`, {
-        status: 500,
-      })
-    }
-  }
-
   try {
-    const {
-      data: { user },
-    } = await supabase.auth.getUser()
-    if (!user) return new Response("No autorizado", { status: 401 })
-
-    const existingRegistration = await getRegistrationByUser(supabase, user.id, String(event_slug))
-    let isNewRegistration = false
-
-    if (!existingRegistration) {
-      await submitRegistration(supabase, {
-        event_id: String(event_id),
-        event_slug: String(event_slug),
-        fields,
-      })
-      isNewRegistration = true
+    if (first_name && last_name && phone_number) {
+      await createProfile(supabase, String(first_name), String(last_name), String(phone_number))
     }
 
-    // TODO: replace with joinTeam func
-    console.log(`Uniendose con team code: ${team_code} para el evento: ${event_slug}`)
+    await submitRegistration(supabase, {
+      event_id: String(event_id),
+      event_slug: String(event_slug),
+      fields,
+    })
 
-    if (isNewRegistration) {
-      const userProfile = await getProfile(supabase)
+    joinTeam(supabase, {
+      code: String(team_code),
+      event_id: Number(event_id),
+    })
 
-      if (userProfile && "email" in userProfile) {
-        await sendRegistrationConfirmationEmail({
-          userEmail: userProfile.email,
-          userName: userProfile.first_name ?? "",
-          eventName: String(event_name),
-          eventSlug: String(event_slug),
-        })
-      } else {
-        console.error("No se pudo obtener el perfil o el email del usuario para enviar el correo.")
-      }
+    const userProfile = await getProfile(supabase)
+
+    if (userProfile && "email" in userProfile) {
+      await sendRegistrationConfirmationEmail({
+        userEmail: userProfile.email,
+        userName: userProfile.first_name ?? "",
+        eventName: String(event_name),
+        eventSlug: String(event_slug),
+      })
+    } else {
+      console.error("No se pudo obtener el perfil o el email del usuario para enviar el correo.")
     }
 
     return new Response("Registro exitoso", { status: 200 })
-  } catch (error: any) {
-    return new Response(`Error al registrar y unirse al equipo: ${error.message || error}`, {
-      status: 500,
-    })
+  } catch (error) {
+    return new Response(
+      `Error al registrar y unirse al equipo: ${error instanceof Error ? error.message : error}`,
+      {
+        status: 500,
+      }
+    )
   }
 }
