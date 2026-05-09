9a09eed035e22f44e126dd7d21a40e004056acad
pablonoya <pablonoya18@gmail.com>
2026-05-09 01:44:22 -0400

    Get organizers from DB

--- src/components/events/bwai-26/organizers/OrganizerCard.tsx
+++ src/components/events/bwai-26/organizers/OrganizerCard.tsx
@@ -1,59 +1,42 @@
 const background = "/assets/figma/bg-organizer.svg"
 
 export interface Props {
-  imageUrl: string | null
-  img: string
-  full_name: string
-  carrera: string
-  areas?: string[]
+  image: string | null
+  first_name: string
+  last_name: string
+  areas: string[]
 }
 
-export const OrganizerCard = ({ img, full_name, areas }: Props) => {
+export const OrganizerCard = ({ image, first_name, last_name, areas }: Props) => {
   return (
     <div
-      className="card-speaker flex flex-col items-center rounded-lg overflow-hidden w-[260px] h-[374px] py-4 px-3 bg-cover bg-center bg-no-repeat mx-auto"
+      className="flex flex-col items-center rounded-lg overflow-hidden w-65 h-[374px] py-4 px-3 bg-cover bg-center bg-no-repeat mx-auto"
       style={{ backgroundImage: `url(${background})` }}
     >
       <div className="w-full flex justify-center">
         <img
-          src={img}
-          alt={full_name}
-          className="border border-2 border-black w-56 h-56 object-cover rounded-lg"
+          src={image}
+          alt={first_name}
+          className="border border-black w-56 h-56 object-cover rounded-lg"
           loading="lazy"
           decoding="async"
         />
       </div>
 
-      <div className="pt-2 flex flex-col items-center justify-center text-center flex-grow w-full gap-2 mt-4">
-        <h3 className="text-lg font-semibold leading-tight">
-          {full_name.split(" ").length > 2 ? (
-            <>
-              {full_name.split(" ").slice(0, 2).join(" ")} <br />
-              {full_name.split(" ").slice(2).join(" ")}
-            </>
-          ) : full_name.split(" ").length === 2 ? (
-            <>
-              {full_name.split(" ")[0]} <br />
-              {full_name.split(" ")[1]}
-            </>
-          ) : (
-            <>
-              {full_name} <br />
-              {"\u200B"}
-            </>
-          )}
+      <div className="flex flex-col items-center justify-center text-center w-full gap-2 mt-4">
+        <h3 className="pt-4 text-lg font-semibold leading-tight w-40">
+          {first_name} {last_name.split(" ")[0]}
         </h3>
 
         <div className="flex flex-wrap justify-center gap-3 w-full mt-2">
-          {areas &&
-            areas.map(area => (
-              <span
-                key={area}
-                className="inline-block bg-[#4285F4] rounded-lg px-2 py-1 text-xs font-bold border text-white"
-              >
-                {area}
-              </span>
-            ))}
+          {areas?.map(area => (
+            <span
+              key={area}
+              className="inline-block bg-blue-500 rounded-lg px-2 py-1 text-xs font-bold border text-white"
+            >
+              {area}
+            </span>
+          ))}
         </div>
       </div>
     </div>
