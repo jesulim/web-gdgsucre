b829e9e872ddffbc3c3f8f47eb56489519f0ec1e
pablonoya <pablonoya18@gmail.com>
2026-05-30 17:28:51 -0400

    Add initial landing on index, migrate to Astro v6 and Google Sans Flex font

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,36 +8,16 @@ import { defineConfig, fontProviders } from "astro/config"
 export default defineConfig({
   output: "server",
 
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.local(),
-        name: "Google Sans Display",
-        cssVariable: "--font-google",
-        options: {
-          variants: [
-            {
-              weight: "400",
-              style: "normal",
-              src: ["./src/assets/fonts/GoogleSansDisplay-Regular.ttf"],
-            },
-            {
-              weight: "700",
-              style: "normal",
-              src: ["./src/assets/fonts/GoogleSansDisplay-Bold.ttf"],
-            },
-          ],
-        },
-      },
-    ],
-  },
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Google Sans Flex",
+      cssVariable: "--font-google",
+    },
+  ],
 
   vite: {
     plugins: [tailwindcss()],
-    // FIXME: Issue with Astro 5.15 https://github.com/withastro/astro/issues/14692#issuecomment-3487416407
-    optimizeDeps: {
-      include: ["picocolors"],
-    },
   },
 
   integrations: [react()],
